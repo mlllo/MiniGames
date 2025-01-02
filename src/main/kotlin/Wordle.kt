b9acdd0f3d440cdbc5f6c3725d6b6863843766e9
mlllo <milromanowski@gmail.com>
2025-01-02 12:52:37 +0100

    !

--- src/main/kotlin/Wordle.kt
+++ src/main/kotlin/Wordle.kt
@@ -4,42 +4,40 @@ import java.util.*
 
 fun main() {
     val dWord = DummyDictionary();
-    val word = dWord.getRandomWord().uppercase(Locale.getDefault());
+    var word = dWord.getRandomWord().uppercase(Locale.getDefault());
     val wordLength = word.length;
     val scanner = Scanner(System.`in`);
+    var isRunning = true;
 
     println("If you want exit write 'exit'.\nWord have: $wordLength letters");
-    while (true) {
+
+    while (isRunning) {
         var answer = scanner.nextLine();
 
-        if(answer.equals("new", ignoreCase = true)) {
-            dWord.getRandomWord();
-            return main();
-        }else if (answer.equals("current", ignoreCase = true)) {
-            println(word);
-            println("Answer:");
-            continue;
-        }else if(answer.equals("exit", ignoreCase = true)) {
-            return;
-        }
-
-        if(answer.equals(word, ignoreCase = true)) {
+        if (answer.equals("new", ignoreCase = true)) {
+            word = dWord.getRandomWord().uppercase(Locale.getDefault());
+            println("Word have: ${word.length} letters")
+        } else if (answer.equals("current", ignoreCase = true)) {
+            println("Answer: $word");
+        } else if (answer.equals("exit", ignoreCase = true)) {
+            isRunning = false;
+        } else if (answer.equals(word, ignoreCase = true)) {
             println("You win! Do you want to try again? Write yes/no.");
-            answer = scanner.nextLine()
-            if(answer.equals("yes", ignoreCase = true)) {
-                dWord.getRandomWord();
-                return main();
-            }else if(answer.equals("no", ignoreCase = true)) {
-                return;
-            }else {
-                println("UnCorrect!");
+            answer = scanner.nextLine();
+            if (answer.equals("yes", ignoreCase = true)) {
+                word = dWord.getRandomWord().uppercase(Locale.getDefault());
+                println("Word have: ${word.length} letters")
+            } else if (answer.equals("no", ignoreCase = true)) {
+                isRunning = false;
+            } else {
+                println("Unknown option!");
             }
-        }else if(!answer.equals(word, ignoreCase = true)){
+        } else if(!answer.equals(word, ignoreCase = true)){
             println("Try again!");
             val hash = "⬛".repeat(wordLength);
             val hashMsg = hash.toCharArray();
 
-            if(answer.length != wordLength) {
+            if (answer.length != wordLength) {
                 println("Word must have $wordLength letters!");
                 continue;
             }
@@ -53,9 +51,12 @@ fun main() {
                 }
                 x++;
             }
-            println(hashMsg);
-        }
 
+            println(hashMsg);
+        } else {
+            println("Unknown command!");
+        }
     }
 
+    scanner.close();
 }
